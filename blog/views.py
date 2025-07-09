1965a331f7eee1cb081d29c9f1a869f76deb58fc
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-09 22:00:54 +0530

    feat: add templates for home and about pages

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Ashiq',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Mohamed',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About' })
